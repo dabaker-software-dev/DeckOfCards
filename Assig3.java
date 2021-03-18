595472b9de6010404620dbdef5d9558386e9119f
Franco <47805847+francobel@users.noreply.github.com>
2021-03-17 18:17:55 -0700

    Update Assig3.java

--- Assig3.java
+++ Assig3.java
@@ -1,12 +1,10 @@
 
 public class Assig3 
 {
-
    public static void main(String[] args) 
    {
       
    }
-
 }
 
 class Card
@@ -20,49 +18,82 @@ class Card
    private Suit suit;
    private boolean errorFlag;
    
+   //Constructor sets card to default variables
    public Card() 
    {
-      
+      set('A', Suit.spades);
    }
    
+   //Constructor sets card variables to user input
    public Card(char value, Suit suit) 
    {
-      
+      set(value, suit);
    }
    
-   public string toString()
+   //Get card and error message strings ready to display depending on validity 
+   public String toString()
    {
-      
+      if(errorFlag == true)
+         return "** illegal **";
+      else
+         return getValue() + " " + "of" + " " + getSuit();
    }
    
+   //Set values and return boolean depending on if card is valid
    public boolean set(char value, Suit suit)
    {
+      if(isValid(value, suit))
+      {
+         this.value = value;
+         this.suit = suit;
+         errorFlag = false;
+      }
+      else
+         errorFlag = true;
       
+      return errorFlag; 
    }
    
+   //Return suit
    public Suit getSuit()
    {
-      
+      return suit;
    }
    
-   public Suit getValue()
+   //Return value
+   public char getValue()
    {
-      
+      return value;
    }
    
-   public Suit getErrorFlag()
+   //Return errorFlag
+   public boolean getErrorFlag()
    {
-      
+      return errorFlag;
    }
    
+   //Compare two cards to one another and return boolean
    public boolean equals(Card card)
    {
-      
+      if(card.getValue() == getValue() && card.getSuit() == getSuit() && 
+         card.getErrorFlag() == getErrorFlag())
+      {
+         return true;
+      }
+      else
+         return false;
    }
    
+   //Determine if card value is valid and return boolean
    private boolean isValid(char value, Suit suit)
    {
-      
+      if(value == 'A' || value == '2' || value == '3' || value == '4' || 
+         value == '5' || value == '6' || value == '7' || value == '8' ||
+         value == '9' || value == 'T' || value == 'J' || value == 'Q' ||
+         value == 'K')
+         return true;
+      else
+         return false;
    }
 }
 
